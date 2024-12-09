b5a1abbd72c05195823069a0049a41b37a7eac4e
jjennyvc <153659964+jjennyvc@users.noreply.github.com>
2024-12-09 19:17:30 +0000

    skibidi toilet

--- Main.java
+++ Main.java
@@ -4,6 +4,19 @@ public class Main
 {
   public static void main(String[] args)
   {
-    // Create objects and test your classes in here
+    Point2D p1 = new Point2D();
+    Point2D p2 = new Point2D();
+
+    p1.setX(5);
+    p2.setY(10);
+
+    p1.setX(10);
+    p2.setY(5);
+
+
+    Dog d = new Dog();
+    d.breed = "blabla";
+    d.color = "brown";
+    d.size = 23.7;
   }
 }
